0ce0a46bae7bdf2b66ffc69f4e5cba0342838ef2
Tanooshri Vajpayee <tanooshrivajpayee06@gmail.com>
2023-02-04 16:07:46 +0530

    taking input and console in js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var username = prompt("give your name");
-alert("Welcome "+ username);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+function clickEventhandler(){
+    console.log("clicked!")
+    console.log("input", txtInput.value)
+}
+btnTranslate.addEventListener("click", clickEventhandler )
